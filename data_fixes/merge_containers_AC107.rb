4e904216b98c40fd567a0c2a791ebd8947f8c7eb
regineheberlein <regineheberlein@gmail.com>
2021-04-28 14:36:16 -0400

    return uri/indicator hashes

--- data_fixes/merge_containers_AC107.rb
+++ data_fixes/merge_containers_AC107.rb
@@ -1,5 +1,6 @@
 require 'archivesspace/client'
 require_relative 'sandbox_auth'
+require 'json'
 
 #configure access
 config = ArchivesSpace::Configuration.new({
@@ -16,8 +17,9 @@ config = ArchivesSpace::Configuration.new({
 client = ArchivesSpace::Client.new(config).login
 
 #get all container ids for AC107.xx
-#file_ids = [2044, 2140, 2141, 2152, 2158, 2159, 2160, 2161, 2162, 2163, 2164, 2142, 2143, 2144, 2145, 2146, 2147]
-#file_ids = [2152]
+file_ids = [2044, 2140, 2141, 2152, 2158, 2159, 2160, 2161, 2162, 2163, 2164, 2142, 2143, 2144, 2145, 2146, 2147]
+#file_ids = [2044]
+
 #this doesn't scale
 #containers_all = []
 #file_ids.each do |file|
@@ -27,19 +29,27 @@ client = ArchivesSpace::Client.new(config).login
 #    }}).parsed
 #end
 
-#Lyrasis recommends this
-file_ids = [2044, 2140, 2141, 2152, 2158, 2159, 2160, 2161, 2162, 2163, 2164, 2142, 2143, 2144, 2145, 2146, 2147]
-top_container_uris = []
+#Lyrasis recommends this instead
+#top_container_uris = client.get(
+#  'repositories/4/top_containers/search',
+#  query: { q: 'collection_uri_u_sstr:"/repositories/4/resources/2152"' }
+#).parsed['response']['docs'].map { |result| { "ref" => result['uri'] } }
+
+#top_container_uris.each do |uri|
+#  puts uri
+#end
+
+#search across all the top containers in the repository for AC107; resolve top_container record; put in array
+top_containers = []
 file_ids.each do |id|
-  top_container_uris << client.get(
+  top_containers << client.get(
   'repositories/4/top_containers/search',
   #need to use two sets of double quotes to allow query parameters to use interpolation
   query: { q: "collection_uri_u_sstr:\"/repositories/4/resources/#{id}\"" }
-  ).parsed['response']['docs'].map { |result| { "ref" => result['uri'] } }
-  end
-
-top_container_uris.each do |uri|
-  puts uri
+  #the json field, counter-intuitively, is a string; parse as json
+).parsed['response']['docs'].map { |result| { result['uri'] => JSON.parse(result['json'])['indicator']} }
 end
 
-puts top_container_uris
+top_containers.each do |ref|
+  puts ref
+end
