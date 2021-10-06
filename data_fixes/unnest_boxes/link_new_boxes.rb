abc40c6c370b14775a7406895c1a4ad9af3f6252
regineheberlein <regineheberlein@gmail.com>
2021-10-05 20:27:44 -0400

    link newly created boxes to their respective archival objects

---
+++ data_fixes/unnest_boxes/link_new_boxes.rb
@@ -0,0 +1,52 @@
+require 'archivesspace/client'
+require 'json'
+require_relative '../../helper_methods.rb'
+
+aspace_staging_login()
+
+start_time = "Process started: #{Time.now}"
+puts start_time
+
+# #declare input file with uri and restriction value
+csv = CSV.parse(File.read("test_linking.csv"), :headers => true)
+log = "link_nested_boxes_log.txt"
+
+csv.each do |row|
+  uri = row['ao_uri']
+  ao = @client.get(uri).parsed
+    ao['instances'] <<
+      {"instance_type"=>"mixed_materials",
+        "jsonmodel_type"=>"instance",
+        "sub_container"=>{
+          "jsonmodel_type"=>"sub_container",
+          "top_container"=>{"ref"=>"#{row['container_uri']}"}
+        }
+    }
+  post = @client.post(uri, ao.to_json)
+  response = post.body
+  puts response
+  File.write(log, response, mode: 'a')
+rescue Exception => msg
+end_time = "Process ended: #{Time.now} with message '#{msg.class}: #{msg.message}''"
+
+end
+
+puts "Process ended: #{Time.now}"
+
+# "instances"=>[{"lock_version"=>0,
+# "created_by"=>"admin",
+# "last_modified_by"=>"admin",
+# "create_time"=>"2021-01-23T02:09:49Z",
+# "system_mtime"=>"2021-01-23T02:09:49Z",
+# "user_mtime"=>"2021-01-23T02:09:49Z",
+# "instance_type"=>"mixed_materials",
+# "jsonmodel_type"=>"instance",
+# "is_representative"=>false,
+# "sub_container"=>{"lock_version"=>0,
+# "created_by"=>"admin",
+# "last_modified_by"=>"admin",
+# "create_time"=>"2021-01-23T02:09:49Z",
+# "system_mtime"=>"2021-01-23T02:09:49Z",
+# "user_mtime"=>"2021-01-23T02:09:49Z",
+# "jsonmodel_type"=>"sub_container",
+# "top_container"=>{"ref"=>"/repositories/3/top_containers/31949"}}}]
