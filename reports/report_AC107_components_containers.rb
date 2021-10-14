f2870613e487cab1e2e3a5f86d817e93872f19b0
regineheberlein <regineheberlein@gmail.com>
2021-10-14 08:07:40 -0400

    remove obsolete comments, add explanatory comments

--- reports/report_AC107_components_containers.rb
+++ reports/report_AC107_components_containers.rb
@@ -13,8 +13,6 @@ file_ids = [2044, 2140, 2141, 2152, 2158, 2159, 2160, 2161, 2162, 2163, 2164, 21
 #endpoint for ao's within a collection:
 # /repositories/:repo_id/resources/:id/ordered_records
 
-#file_ids = [2146, 2147]
-#aos = get_all_records_for_repo_endpoint(12, "archival_objects")
 responses = []
 aos = []
 file_ids.each do |id|
@@ -32,34 +30,35 @@ file_ids.each do |id|
   puts "Finished gathering components of file id #{id} at #{Time.now}."
 end #end file_ids.each
 
-
-  CSV.open("test.csv", "a",
-    :write_headers=> true,
-    :headers => ["uri", "ref_id", "level", "title", "instances"]) do |row|
-    aos.each do |ao|
-        top_containers2csv = String.new
-        top_containers = unless ao['instances'].nil?
-          ao['instances'].each do |instance|
-            subcontainer_type = if instance.dig('sub_container', 'type_2') then instance['sub_container']['type_2'] end
-            subcontainer_indicator = if instance.dig('sub_container', 'indicator_2') then instance['sub_container']['indicator_2'] end
-            unless subcontainer_type.nil?
-              subcontainer = "#{subcontainer_type} #{subcontainer_indicator} "
-            end #end unless
-            unless instance['sub_container']['top_container']['ref'].nil?
-              top_container = @client.get(instance['sub_container']['top_container']['ref']).parsed
-              top_containers2csv << "#{top_container['uri']}: #{top_container['type']} #{top_container['indicator']} #{subcontainer}; "
-            end #end unless
-          end #end ao['instances'].each
-          puts "#{ao['uri']}, #{ao['ref_id']}, #{ao['level']}, #{ao['title']}, #{top_containers2csv}"
-          row << [ao['uri'], ao['ref_id'], ao['level'], ao['title'], top_containers2csv]
-        else
-          puts "#{ao['uri']}, #{ao['ref_id']}, #{ao['level']}, #{ao['title']}"
-          row << [ao['uri'], ao['ref_id'], ao['level'], ao['title']]
-        end #end unless
-    rescue Exception => msg
-    end_time = "Process interrupted at #{Time.now} with message '#{msg.class}: #{msg.message}''"
-    puts end_time
-    end #end aos.each
-  end #end row builder
+#iterate over records, get ao and subcontainer data, then hit the API for each topcontainer
+#write everything to csv
+CSV.open("AC107.csv", "a",
+  :write_headers=> true,
+  :headers => ["uri", "ref_id", "level", "title", "instances"]) do |row|
+  aos.each do |ao|
+      top_containers2csv = String.new
+      top_containers = unless ao['instances'].nil?
+        ao['instances'].each do |instance|
+          subcontainer_type = if instance.dig('sub_container', 'type_2') then instance['sub_container']['type_2'] end
+          subcontainer_indicator = if instance.dig('sub_container', 'indicator_2') then instance['sub_container']['indicator_2'] end
+          unless subcontainer_type.nil?
+            subcontainer = "#{subcontainer_type} #{subcontainer_indicator} "
+          end #end unless
+          unless instance['sub_container']['top_container']['ref'].nil?
+            top_container = @client.get(instance['sub_container']['top_container']['ref']).parsed
+            top_containers2csv << "#{top_container['uri']}: #{top_container['type']} #{top_container['indicator']} #{subcontainer}; "
+          end #end unless
+        end #end ao['instances'].each
+        puts "#{ao['uri']}, #{ao['ref_id']}, #{ao['level']}, #{ao['title']}, #{top_containers2csv}"
+        row << [ao['uri'], ao['ref_id'], ao['level'], ao['title'], top_containers2csv]
+      else
+        puts "#{ao['uri']}, #{ao['ref_id']}, #{ao['level']}, #{ao['title']}"
+        row << [ao['uri'], ao['ref_id'], ao['level'], ao['title']]
+      end #end unless
+  rescue Exception => msg
+  end_time = "Process interrupted at #{Time.now} with message '#{msg.class}: #{msg.message}''"
+  puts end_time
+  end #end aos.each
+end #end row builder
 
 puts "Process ended: #{Time.now}"
