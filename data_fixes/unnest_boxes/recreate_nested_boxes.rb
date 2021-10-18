f67e3353ef802ce99065458ddb7496dbaca22247
regineheberlein <regineheberlein@gmail.com>
2021-10-18 15:19:09 -0400

    write existing containers to log

--- data_fixes/unnest_boxes/recreate_nested_boxes.rb
+++ data_fixes/unnest_boxes/recreate_nested_boxes.rb
@@ -8,11 +8,11 @@ start_time = "Process started: #{Time.now}"
 puts start_time
 
 # #declare input file with uri and restriction value
-csv = CSV.parse(File.read("test-restriction.csv"), :headers => true)
-log = "restrictions_log.txt"
+csv = CSV.parse(File.read("input_recreate_nested_boxes.csv"), :headers => true)
+log = "recreate_nested_boxes_log.txt"
 
-#containers_all = get_all_top_container_records_for_institution()
-containers_all = get_all_records_for_repo_endpoint(12, 'top_containers')
+containers_all = get_all_top_container_records_for_institution()
+#containers_all = get_all_records_for_repo_endpoint(12, 'top_containers')
 existing_container_ids = []
   containers_all.each do |container|
   existing_container_ids << {container['ils_holding_id'] => container['uri']}
@@ -66,9 +66,11 @@ top_containers.each do |top_container|
     response = JSON.parse post.body
     response_parsed = "#{top_container['ils_holding_id']}:#{response['uri']}:created\n"
     puts response_parsed
-  else puts "#{existing_container.keys[0]}:#{existing_container.values[0]}:already exists\n"
+  else no_response = "#{existing_container.keys[0]}:#{existing_container.values[0]}:already exists\n"
+    puts no_response
     end
-    File.write(log, post.body, mode: 'a')
+    log_entry = if response then response_parsed else no_response
+    File.write(log, log_entry, mode: 'a')
   rescue Exception => msg
   end_time = "Process ended: #{Time.now} with message '#{msg.class}: #{msg.message}''"
   puts end_time
