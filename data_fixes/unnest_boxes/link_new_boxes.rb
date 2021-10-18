4f28f6b23bcbd613b9520ec670ea1b0a8979400d
regineheberlein <regineheberlein@gmail.com>
2021-10-18 15:19:18 -0400

    check whether the container is already linked

--- data_fixes/unnest_boxes/link_new_boxes.rb
+++ data_fixes/unnest_boxes/link_new_boxes.rb
@@ -8,13 +8,20 @@ start_time = "Process started: #{Time.now}"
 puts start_time
 
 # #declare input file with uri and restriction value
-csv = CSV.parse(File.read("test-restriction.csv"), :headers => true)
+csv = CSV.parse(File.read("input_link_new_boxes.csv"), :headers => true)
 log = "link_nested_boxes_log.txt"
 
 csv.each do |row|
   puts row['ao_uri']
   uri = row['ao_uri']
   ao = @client.get(uri).parsed
+    ao_instance_uris = []
+    ao_instance_uris << ao['instances'].each do |instance|
+      if instance['subcontainer']
+        then instance['subcontainer']['top_container']['ref']
+      end
+    end
+    unless ao_instance_uris.include?(row['container_uri'])
     ao['instances'] <<
       {"instance_type"=>"mixed_materials",
         "jsonmodel_type"=>"instance",
@@ -27,10 +34,12 @@ csv.each do |row|
   response = post.body
   puts response
   File.write(log, response, mode: 'a')
+  end
 rescue Exception => msg
 end_time = "Process ended: #{Time.now} with message '#{msg.class}: #{msg.message}''"
 puts end_time
 end
+
 
 puts "Process ended: #{Time.now}"
 
