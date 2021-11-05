47a8a2573a6bb844ed149374d07df150f9675ddd
regineheberlein <regineheberlein@gmail.com>
2021-11-04 20:11:15 -0400

    fix filename variable, path to helpers

--- data_fixes/remove_ao_restrictions/get_restrictions2input.rb
+++ data_fixes/remove_ao_restrictions/get_restrictions2input.rb
@@ -1,16 +1,16 @@
 require 'archivesspace/client'
 require 'json'
 require 'csv'
-require_relative 'helper_methods.rb'
+require_relative '../../helper_methods.rb'
 
 aspace_login()
 
 start_time = "Process started: #{Time.now}"
 puts start_time
 
-filename = 'test.csv'
+filename = "get_aos_4.csv"
 #repos_all = (7..12).to_a
-repos_all = [11]
+repos_all = [4]
 aos = []
 
 CSV.open(filename, "wb",
