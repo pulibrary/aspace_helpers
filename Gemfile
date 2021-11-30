5328f5c4df62c49c5d76372cb6410570cb229ee6
Carolyn Cole <cac9@princeton.edu>
2021-11-30 09:08:07 -0500

    Adding linked file and gems

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
+source "https://rubygems.org"
+
+gem 'archivesspace-client'
+gem 'rake'
+
 group :development do
   gem "capistrano", "~> 3.16", require: false
+  gem "capistrano-bundler"
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/opt/aspace_helpers"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "sandbox_auth.rb"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
