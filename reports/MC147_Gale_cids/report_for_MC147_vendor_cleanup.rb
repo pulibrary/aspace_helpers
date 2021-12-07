43e3cd1c563a62e2dc3445e8cbac5dd4cdab3756
regineheberlein <regineheberlein@gmail.com>
2021-12-07 13:32:34 -0500

    I don't remember what changed in here, but just in case

--- reports/MC147_Gale_cids/report_for_MC147_vendor_cleanup.rb
+++ reports/MC147_Gale_cids/report_for_MC147_vendor_cleanup.rb
@@ -46,10 +46,9 @@ ao_tree['uris'].each do |ao_ref|
                 end
                 "#{sub1} #{sub2}"
             end
-              end #unless
-              row << [get_ao['uri'], get_ao['ref_id'], get_ao['title'], get_ao['dates'][0]['expression'], "#{top_container unless top_container.nil?} #{sub_container unless sub_container.nil?}"]
-              puts "#{get_ao['uri']}, #{get_ao['ref_id']}, #{get_ao['title']}, #{get_ao['dates'][0]['expression']}, #{top_container unless top_container.nil?} #{sub_container unless sub_container.nil?}"
-
+          end #unless
+          row << [get_ao['uri'], get_ao['ref_id'], get_ao['title'], get_ao['dates'][0]['expression'], "#{top_container unless top_container.nil?} #{sub_container unless sub_container.nil?}"]
+          puts "#{get_ao['uri']}, #{get_ao['ref_id']}, #{get_ao['title']}, #{get_ao['dates'][0]['expression']}, #{top_container unless top_container.nil?} #{sub_container unless sub_container.nil?}"
         end #uri.each
       rescue Exception => msg
       end_time = "Process interrupted at #{Time.now} with message '#{msg.class}: #{msg.message}''"
