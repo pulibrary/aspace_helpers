f7a0acf82ea0183b6114b3a25784803977719e10
regineheberlein <regineheberlein@users.noreply.github.com>
2021-12-07 14:20:56 -0500

    Report mc147 (#104)
    * add containers; code cleanup: move container variables outside unless loop; move return outside unless loop; simplify top_container return; remove inactive variable top_containers; remove unnecessary 'then'
    
    * don't return nil containers
    
    * quick and dirty way to return individual records for each folder in a range, e.g. '1-32'
    
    * just in case we need these again
    
    * I don't remember what changed in here, but just in case

--- reports/MC147_Gale_cids/report_for_MC147_vendor_cleanup.rb
+++ reports/MC147_Gale_cids/report_for_MC147_vendor_cleanup.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'csv'
 require_relative '../../helper_methods.rb'
 
-aspace_staging_login()
+aspace_login()
 
 start_time = "Process started: #{Time.now}"
 puts start_time
@@ -13,27 +13,48 @@ resource_id = "1650"
 repo = 3
 output_file = "#{eadid}.csv"
 ao_tree = @client.get("/repositories/#{repo}/resources/#{resource_id}/ordered_records").parsed
-#/repositories/:repo_id/resources/:id/ordered_records
-#get_all_archival_objects_for_resource(4, 4185, ['top_containers'])
 
-  CSV.open(output_file, "a",
-    :write_headers=> true,
-    :headers => ["uri", "ref_id", "title", "first_date"]) do |row|
-    ao_tree['uris'].each do |ao_ref|
-      ao_uri = []
-      ao_uri << ao_ref['ref'] unless ao_ref.dig('level') == 'collection'
-      #puts ao_uri
+CSV.open(output_file, "a",
+  :write_headers=> true,
+  :headers => ["uri", "ref_id", "title", "first_date", "container"]) do |row|
+ao_tree['uris'].each do |ao_ref|
+  ao_uri = []
+  ao_uri << ao_ref['ref'] unless ao_ref.dig('level') == 'collection'
+  #puts ao_uri
 
-    ao_uri.each do |uri|
-      #puts uri.class
-      get_ao = @client.get(uri).parsed
-        row << [get_ao['uri'], get_ao['ref_id'], get_ao['title'], get_ao['dates'][0]['expression']]
-        puts "#{get_ao['uri']}, #{get_ao['ref_id']}, #{get_ao['title']}, #{get_ao['dates'][0]['expression']}"
-    end #uri.each
-        rescue Exception => msg
-        end_time = "Process interrupted at #{Time.now} with message '#{msg.class}: #{msg.message}''"
-      end #csv
-    end #tree.each
+  ao_uri.each do |uri|
+    #puts uri.class
+    get_ao = @client.get(uri).parsed
+    top_container = nil
+    sub_container = nil
+      unless get_ao['instances'].nil?
+        get_ao['instances'].each do |instance|
+          top_container_record =
+            if instance.dig('sub_container').nil? == false
+              @client.get(instance['sub_container']['top_container']['ref']).parsed
+              else
+                if instance.dig('top_container')
+                  @client.get(instance['top_container']['ref']).parsed
+                end
+            end
+          top_container = "#{top_container_record['type']} #{top_container_record['indicator']}"
+          sub_container =
+            if instance.dig('sub_container').nil? == false
+                sub1 = instance['sub_container']['type_2'] + " " + instance['sub_container']['indicator_2']
+                unless instance.dig('sub_container', 'type_3').nil?
+                  sub2 = instance.dig('sub_container', 'type_3') + instance.dig('sub_container', 'indicator_3')
+                end
+                "#{sub1} #{sub2}"
+            end
+          end #unless
+          row << [get_ao['uri'], get_ao['ref_id'], get_ao['title'], get_ao['dates'][0]['expression'], "#{top_container unless top_container.nil?} #{sub_container unless sub_container.nil?}"]
+          puts "#{get_ao['uri']}, #{get_ao['ref_id']}, #{get_ao['title']}, #{get_ao['dates'][0]['expression']}, #{top_container unless top_container.nil?} #{sub_container unless sub_container.nil?}"
+        end #uri.each
+      rescue Exception => msg
+      end_time = "Process interrupted at #{Time.now} with message '#{msg.class}: #{msg.message}''"
+      end #get_ao['instances'].each
+    end #csv
+  end #tree.each
 
 end_time = "Process ended: #{Time.now}"
 puts end_time
