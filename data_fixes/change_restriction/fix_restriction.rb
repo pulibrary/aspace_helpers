ae9a9b8010dc4c7b82544a465da6749e20626650
regineheberlein <regineheberlein@gmail.com>
2022-02-18 09:15:30 -0500

    adding end date template

--- data_fixes/change_restriction/fix_restriction.rb
+++ data_fixes/change_restriction/fix_restriction.rb
@@ -9,16 +9,18 @@ aspace_staging_login()
 start_time = "Process started: #{Time.now}"
 puts start_time
 
-log = "log_set_to_open.csv"
-csv = CSV.parse(File.read("set_to_open.csv"), :headers => true)
+log = "log_set_to_open_with_note.csv"
+csv = CSV.parse(File.read("set_to_open_with_note.csv"), :headers => true)
 
   csv.each do |row|
     record = @client.get(row['uri']).parsed
     accessrestrict = record['notes'].select { |note| note["type"] == "accessrestrict" }
-    #change restriction type here
-    accessrestrict[0]['rights_restriction']['local_access_restriction_type'] = ["Open"]
-    #change restriction note here
-    accessrestrict[0]['subnotes'][0]['content'] = "this is a test"
+    #change restriction type here. Takes an array.
+    accessrestrict[0]['rights_restriction']['local_access_restriction_type'] = row['restriction_type']
+    #change restriction note here. Takes a string.
+    accessrestrict[0]['subnotes'][0]['content'] = row['restriction_note']
+    #change the end date here
+    accessrestrict[0]['rights_restriction']['end'] = row['end_date']
     post = @client.post(row['uri'], record.to_json)
     #write to log
     File.write(log, post.body, mode: 'a')
@@ -27,4 +29,4 @@ csv = CSV.parse(File.read("set_to_open.csv"), :headers => true)
     error = "Process ended: #{Time.now} with error '#{msg.class}: #{msg.message}''"
   end
 
-    puts "Process ended #{Time.now}."
+puts "Process ended #{Time.now}."
