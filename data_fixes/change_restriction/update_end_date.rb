71edbbf5a8857ae2cce24dd121f34f57ab67f90a
regineheberlein <regineheberlein@gmail.com>
2022-02-21 15:07:16 -0500

    update type, note, and end date; restriction might exist and need to be overwritten; restriction might not exist but other notes might so it needs to be added to array; no other notes might exist

--- data_fixes/change_restriction/update_end_date.rb
+++ data_fixes/change_restriction/update_end_date.rb
@@ -4,29 +4,47 @@ require 'csv'
 require 'pony'
 require_relative '../../helper_methods.rb'
 
-aspace_staging_login()
+aspace_login()
 
 start_time = "Process started: #{Time.now}"
 puts start_time
 
-log = "log_set_to_open_with_note.csv"
-csv = CSV.parse(File.read("set_to_open_with_note.csv"), :headers => true)
+log = "log_set_end_date.csv"
+csv = CSV.parse(File.read("set_end_date.csv"), :headers => true)
+
+#get ao's for one collection
+#ao_tree = @client.get("/repositories/#{repo}/resources/#{resource_id}/ordered_records").parsed
 
   csv.each do |row|
     record = @client.get(row['uri']).parsed
-    accessrestrict = record['notes'].select { |note| note["type"] == "accessrestrict" }
-    #change restriction type here. Takes an array.
-    accessrestrict[0]['rights_restriction']['local_access_restriction_type'] = row['restriction_type']
-    #change restriction note here. Takes a string.
-    accessrestrict[0]['subnotes'][0]['content'] = row['restriction_note']
-    #change the end date here
-    accessrestrict[0]['rights_restriction']['end'] = row['end_date'] unless row['end_date'].blank?
+    #accessrestrict might need to be overwritten or created
+    new_accessrestrict =
+        {
+        "jsonmodel_type"=>"note_multipart",
+        "type"=>"accessrestrict",
+        "rights_restriction"=>{"end"=>row['end_date'],
+        "local_access_restriction_type"=>[row['restriction_type']]},
+        "subnotes"=>[{"jsonmodel_type"=>"note_text",
+        "content"=>row['restriction_note'],
+        "publish"=>true}],
+        "publish"=>true
+        }
+    accessrestrict = record['notes'].select { |note| note["type"] == "accessrestrict" }[0]
+    if accessrestrict.nil? == false
+      accessrestrict = accessrestrict.replace(new_accessrestrict)
+    else
+      if record['notes'].any?
+        then record['notes'] << new_accessrestrict
+      else record['notes'] = [new_accessrestrict]
+      end
+    end
+
     post = @client.post(row['uri'], record.to_json)
     #write to log
     File.write(log, post.body, mode: 'a')
     puts post.body
     rescue Exception => msg
     error = "Process ended: #{Time.now} with error '#{msg.class}: #{msg.message}''"
-  end
+   end
 
 puts "Process ended #{Time.now}."
