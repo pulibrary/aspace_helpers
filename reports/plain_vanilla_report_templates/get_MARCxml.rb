fa549ef6f026ff81f5b22c0fe3087b360524dcfe
regineheberlein <regineheberlein@gmail.com>
2022-02-22 12:34:30 -0500

    this is how Nokogiri does XPath

--- reports/plain_vanilla_report_templates/get_MARCxml.rb
+++ reports/plain_vanilla_report_templates/get_MARCxml.rb
@@ -1,10 +1,12 @@
 require 'archivesspace/client'
 require 'active_support/all'
 require 'nokogiri'
-require_relative 'helper_methods.rb'
+require_relative '../../helper_methods.rb'
 
 aspace_staging_login()
 
 marc_record = @client.get("/repositories/4/resources/marc21/2065.xml")
-marc_document = Nokogiri::XML(marc_record.body)
-puts marc_document
+doc = Nokogiri::XML(marc_record.body)
+#
+# Search for nodes by xpath
+puts doc.xpath('//marc:datafield[@tag="520"]')
