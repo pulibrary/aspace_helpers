09bca594ea04347591a09aa3f61947a03428813b
regineheberlein <regineheberlein@gmail.com>
2022-02-22 14:49:47 -0500

    add 046

--- reports/plain_vanilla_report_templates/get_MARCxml.rb
+++ reports/plain_vanilla_report_templates/get_MARCxml.rb
@@ -19,6 +19,9 @@ end
 
 # set up variables (these may return a sequence)
 ##################
+tag008 = doc.at_xpath('//marc:controlfield[@tag="008"]')
+tags040 = doc.xpath('//marc:datafield[@tag="040"]')
+tag041 = doc.at_xpath('//marc:datafield[@tag="041"]')
 tags544 = doc.xpath('//marc:datafield[@tag="544"]')
 tags852 = doc.xpath('//marc:datafield[@tag="852"]')
 
@@ -27,6 +30,24 @@ tags852 = doc.xpath('//marc:datafield[@tag="852"]')
 
 #addresses github #128
 remove_empty_elements(doc)
+
+#addresses github #131
+tags040.each do |tag040|
+  tag040.replace('<datafield ind1=" " ind2=" " tag="040">
+      <subfield code="a">NjP</subfield>
+      <subfield code="b">eng</subfield>
+      <subfield code="e">dacs</subfield>
+      <subfield code="c">NjP</subfield>
+    </datafield>')
+end
+
+#addresses github #134
+#swap quotes so interpolation is possible
+tag041.next=("<datafield ind1=' ' ind2=' ' tag='046'>
+      <subfield code='a'>i</subfield>
+      <subfield code='c'>#{tag008.content[7..10]}</subfield>
+      <subfield code='e'>#{tag008.content[11..14]}</subfield>
+    </datafield>")
 
 #addresses github #133
 #NB node.children.before inserts new node as first of node's children; default for add_child is last
