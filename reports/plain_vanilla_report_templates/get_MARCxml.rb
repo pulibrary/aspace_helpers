d34755cb0b2732f68ab9047f8b9abc6f2c9c9f7d
regineheberlein <regineheberlein@gmail.com>
2022-02-22 12:57:02 -0500

    this is how Nokogiri updates or conditionally removes an element (NB regardless of attributes)

--- reports/plain_vanilla_report_templates/get_MARCxml.rb
+++ reports/plain_vanilla_report_templates/get_MARCxml.rb
@@ -9,4 +9,12 @@ marc_record = @client.get("/repositories/4/resources/marc21/2065.xml")
 doc = Nokogiri::XML(marc_record.body)
 #
 # Search for nodes by xpath
-puts doc.xpath('//marc:datafield[@tag="520"]')
+tags49 = doc.xpath('//marc:datafield[@tag="049"]')
+tags520 = doc.xpath('//marc:datafield[@tag="520"]')
+
+tags49.each do |tag49|
+  tag49.remove if tag49.xpath('@ind1').empty?
+end
+tags520[0].content = "Snap, Crackle & Pop"
+
+puts doc
