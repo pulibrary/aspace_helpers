b4ec5c72e86a6af9b3414a6368583a06cbd5dfcc
regineheberlein <regineheberlein@users.noreply.github.com>
2022-04-26 14:17:51 -0400

    add logic to create the note if it doesn't exist (#202)

--- data_fixes/change_restriction/fix_restriction.rb
+++ data_fixes/change_restriction/fix_restriction.rb
@@ -1,26 +1,37 @@
 require 'archivesspace/client'
 require 'json'
 require 'csv'
-require 'pony'
 require_relative '../../helper_methods.rb'
 
-aspace_login()
+@client = aspace_staging_login()
 
 start_time = "Process started: #{Time.now}"
 puts start_time
 
-log = "log_set_to_open.csv"
-csv = CSV.parse(File.read("set_to_open.csv"), :headers => true)
+log = "log_aclu_restrictions_20220426.csv"
+csv = CSV.parse(File.read("aclu_restrictions_20220426.csv"), :headers => true)
 
   csv.each do |row|
     record = @client.get(row['uri']).parsed
     accessrestrict = record['notes'].select { |note| note["type"] == "accessrestrict" }
-    #change restriction type here. Takes an array.
-    accessrestrict[0]['rights_restriction']['local_access_restriction_type'] = ["Open"]
-    #change restriction note here. Takes a string.
-    #accessrestrict[0]['subnotes'][0]['content'] = row['restriction_note']
-    #change the end date here
-    #accessrestrict[0]['rights_restriction']['end'] = row['end_date'] unless row['end_date'].empty?
+    unless accessrestrict.empty?
+      #change restriction type here. Takes an array.
+      #accessrestrict[0]['rights_restriction']['local_access_restriction_type'] = ["Open"]
+      accessrestrict[0]['rights_restriction']['local_access_restriction_type'] = [row['restriction_type']]
+      #change restriction note here. Takes a string.
+      accessrestrict[0]['subnotes'][0]['content'] = row['restriction_note']
+      #change the end date here
+      #accessrestrict[0]['rights_restriction']['end'] = row['end_date'] unless row['end_date'].empty?
+    else
+      record['notes'].append(
+          {"type"=>"accessrestrict",
+          "subnotes"=>[{"jsonmodel_type"=>"note_text",
+            "content"=>row['restriction_note'],
+            "publish"=>true}],
+          "jsonmodel_type"=>"note_multipart",
+          "rights_restriction"=>{"local_access_restriction_type"=>[row['restriction_type']]},
+          "publish"=>true})
+    end
     post = @client.post(row['uri'], record.to_json)
     #write to log
     File.write(log, post.body, mode: 'a')
