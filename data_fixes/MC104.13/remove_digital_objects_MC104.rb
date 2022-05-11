6342fb2c3526de856c3fb28a1f4d439e66b0a990
regineheberlein <regineheberlein@gmail.com>
2022-05-11 14:58:31 -0400

    remove superfluous variable

--- data_fixes/MC104.13/remove_digital_objects_MC104.rb
+++ data_fixes/MC104.13/remove_digital_objects_MC104.rb
@@ -13,7 +13,6 @@ puts start_time
 resource = @client.get("/repositories/3/resources/1862/ordered_records").parsed
 #greedy '+'' matches second 's' as intended
 ids = resource['uris'].map {|uri| uri['ref'].gsub!(/^.+s\//, "") unless uri['level'] == 'collection'}
-aos =
   ids.each do |id|
     unless id.nil?
     ao = get_single_archival_object_by_id(3, id, ['digital_object'])
