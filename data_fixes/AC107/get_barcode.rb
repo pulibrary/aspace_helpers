2cead4a9928016e31700f21da91126622750de2e
regineheberlein <regineheberlein@gmail.com>
2023-02-14 20:39:42 -0500

    get barcode/uri mapping

---
+++ data_fixes/AC107/get_barcode.rb
@@ -0,0 +1,16 @@
+require 'archivesspace/client'
+require 'json'
+require_relative '../../helper_methods.rb'
+
+aspace_staging_login()
+
+start_time = "Process started: #{Time.now}"
+puts start_time
+
+csv = CSV.parse(File.read("log_create_top_containers.csv"), :headers => true)
+
+csv.each do |row|
+  uri = row['uri']
+  top_container = @client.get(uri).parsed
+  puts "#{top_container['uri']}, #{top_container['barcode']}"
+end 
