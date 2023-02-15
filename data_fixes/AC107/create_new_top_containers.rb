1c92c6512f4988c8b59e9acd6ca24858698060c6
regineheberlein <regineheberlein@users.noreply.github.com>
2023-02-15 06:44:00 -0500

    Ac107 (#353)
    Create new container records from CSV; map container uri's to ao uri's; link new and existing containers to ao's excluding those already linked

--- data_fixes/AC107/create_new_top_containers.rb
+++ data_fixes/AC107/create_new_top_containers.rb
@@ -3,7 +3,7 @@ require 'active_support/all'
 require 'json'
 require_relative '../../helper_methods.rb'
 
-aspace_staging_login()
+aspace_login()
 
 start_time = "Process started: #{Time.now}"
 puts start_time
