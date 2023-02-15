e5f59c7d27eac10490a4c469259e54de694e52f8
regineheberlein <regineheberlein@gmail.com>
2023-02-14 20:33:37 -0500

    mss container report

--- reports/ReCAP_project/scamss_top_containers.rb
+++ reports/ReCAP_project/scamss_top_containers.rb
@@ -1,22 +1,24 @@
 require 'archivesspace/client'
 require 'active_support/all'
 require 'nokogiri'
-require_relative 'helper_methods.rb'
+require_relative '../../helper_methods.rb'
 
 
 @client = aspace_login
 
-top_container_uris = @client.get(
+top_containers = @client.get(
   'repositories/5/top_containers/search',
-  query: { q: 'location_uri_u_sstr:"/locations/23648"' }
+  query: {
+    q: 'location_uri_u_sstr:"/locations/23648"'
+   }
   ).parsed['response']['docs']
-puts top_container_uris.count
+puts top_containers.count
 
 CSV.open("scamss_repo5_top_containers.csv", "a",
   :write_headers=> true,
   :headers => ["ead_id", "container_profile", "display_string", "uri"]) do |row|
 
-  top_container_uris.map do |result|
+  top_containers.map do |result|
       # "
       # #{result['collection_identifier_stored_u_sstr'][0] unless result['collection_identifier_stored_u_sstr'].nil?} ^
       # #{result['container_profile_display_string_u_sstr'][0]} ^
@@ -25,7 +27,7 @@ CSV.open("scamss_repo5_top_containers.csv", "a",
       # "
       row << [
         "#{result['collection_identifier_stored_u_sstr'][0] unless result['collection_identifier_stored_u_sstr'].nil?}",
-        result['container_profile_display_string_u_sstr'][0],
+        "#{result['container_profile_display_string_u_sstr'][0] unless result['container_profile_display_string_u_sstr'].nil?}",
         result['display_string'],
         result['uri']
       ]
