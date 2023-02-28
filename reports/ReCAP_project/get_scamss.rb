b65b3bb84f31bc99baea06ddc07400986b5ccaae
regineheberlein <regineheberlein@users.noreply.github.com>
2023-02-28 17:03:47 -0500

    report out extent information for scamss location in repo 5 (#346)
    * report out containers in scamss locations for space planning project

---
+++ reports/ReCAP_project/get_scamss.rb
@@ -0,0 +1,32 @@
+require 'archivesspace/client'
+require 'active_support/all'
+require 'nokogiri'
+require_relative 'helper_methods.rb'
+
+@client = aspace_login
+
+resources = get_all_records_for_repo_endpoint(5, 'resources')
+CSV.open("mss_linear_feet.csv", "a",
+  :write_headers=> true,
+  :headers => ["uri", "ead_id", "physloc", "other_physlocs", "linear_feet", "structured_container_count", "unstructured_container_summary"]) do |row|
+  resources.each do |resource|
+    extents = resource['extents']
+    linear_feet = extents.select { |extent| extent['portion'] == 'whole' && extent['extent_type'] == 'linear feet'}
+    structured_container_count = extents.select { |extent| extent['portion'] == 'whole' && extent['extent_type'] != 'linear feet'}
+    notes = resource['notes']
+    physlocs = notes.select { |note| note['type'] == 'physloc'}
+    scamss = physlocs.select { |physloc| physloc['content'][0] == 'scamss'}
+    other_physlocs = physlocs.select { |physloc| physloc['content'][0] != 'scamss' && physloc['content'][0] !~ /\s/}
+    next if scamss.empty?
+
+    row << [
+      resource['uri'],
+      resource['ead_id'],
+      scamss[0]['content'][0],
+      other_physlocs.map { |physloc| physloc['content'][0] }.join(", "),
+      (linear_feet[0]['number'] unless linear_feet.empty?).to_s,
+      ("#{structured_container_count[0]['number']} #{structured_container_count[0]['extent_type']}" unless structured_container_count.empty?).to_s,
+      (linear_feet[0]['container_summary'] unless linear_feet.empty?).to_s
+    ]
+  end
+end
