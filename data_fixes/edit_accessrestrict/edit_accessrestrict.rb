a49822008f021019255b1ec11754f1090d32a47e
regineheberlein <regineheberlein@gmail.com>
2023-11-21 07:53:18 -0500

    update eng access notes; still needs the content of the note

---
+++ data_fixes/edit_accessrestrict/edit_accessrestrict.rb
@@ -0,0 +1,62 @@
+require 'archivesspace/client'
+require 'json'
+require 'csv'
+require_relative '../../helper_methods.rb'
+
+aspace_staging_login
+
+start_time = "Process started: #{Time.now}"
+puts start_time
+
+accessrestrict_type = "test"
+accessrestrict_note = "test"
+
+repos_all = (9..9).to_a
+
+#iterate over all repositories
+repos_all.each do |repo|
+#iterate over all resources within repositories
+resources = get_all_records_for_repo_endpoint(repo, "resources")
+  resources.each do |resource|
+    uri = resource['uri']
+    #filter out all accessrestrict notes
+    accessrestrict_all = resource['notes'].select { |note| note["type"] == "accessrestrict" }
+    #iterate over all access notes
+    accessrestrict_all.each do |accessrestrict|
+        unless accessrestrict.empty?
+            accessrestrict_all.each do |accessrestrict|
+                accessrestrict['rights_restriction']['local_access_restriction_type'] = accessrestrict_type
+                accessrestrict['subnotes'][0]['content'] = accessrestrict_note
+            end
+        else
+            record['notes'].append(
+                {
+                "jsonmodel_type"=>"note_multipart",
+                "type"=>"accessrestrict",
+                "rights_restriction"=>{
+                    "local_access_restriction_type"=>accessrestrict_type
+                },
+                "subnotes"=>[
+                    {
+                    "jsonmodel_type"=>"note_text",
+                    "content"=>accessrestrict_note,
+                    "publish"=>true
+                    }
+                ],
+                "publish"=>true
+                }
+            )
+        end
+    end
+    #write a revision statement to the record at the same time
+    add_resource_revision_statement(resource, "Updated accessrestrict")
+    # post = @client.post(uri, resource.to_json)
+    # puts post.body
+  end
+rescue Exception => msg
+error = "Process ended: #{Time.now} with error '#{msg.class}: #{msg.message}''"
+puts error
+end
+
+end_time = "Process ended: #{Time.now}"
+puts end_time
