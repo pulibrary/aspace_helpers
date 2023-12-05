e691e836e58eeace9e030d984df4b7adf7f2cdec
regineheberlein <regineheberlein@gmail.com>
2023-12-05 09:56:49 -0500

    rubocop

--- data_fixes/edit_userestrict/edit_userestrict_exceptions.rb
+++ data_fixes/edit_userestrict/edit_userestrict_exceptions.rb
@@ -15,40 +15,37 @@ userestrict_note = "test"
 csv.each do |row|
     uri = row['uri']
     resource = @client.get(uri).parsed
-     #filter out all accessrestrict notes
+    #filter out all accessrestrict notes
     userestrict_all = resource['notes'].select { |note| note["type"] == "userestrict" }
     #iterate over all access notes
     userestrict_all.each do |userestrict|
         if userestrict.empty?
             resource['notes'].append(
-            {
-            "jsonmodel_type"=>"note_multipart",
-            "type"=>"userestrict",
-            "subnotes"=>[
+              {
+                "jsonmodel_type"=>"note_multipart",
+              "type"=>"userestrict",
+              "subnotes"=>[
                 {
-                "jsonmodel_type"=>"note_text",
+                  "jsonmodel_type"=>"note_text",
                 "content"=>userestrict_note,
                 "publish"=>true
                 }
-            ],
-            "publish"=>true
-            }
+              ],
+              "publish"=>true
+              }
             )
         else
             userestrict['subnotes'][0]['content'] = userestrict_note
         end
     end
-    #write a revision statement to the record at the same time
-    add_resource_revision_statement(uri, "Updated use restriction.")
-    post = @client.post(uri, resource.to_json)
-    puts post.body
-    File.write(log, post.body, mode: 'a')
-    end
-    rescue Exception => msg
-    error = "Process ended: #{Time.now} with error '#{msg.class}: #{msg.message}''"
-    puts error
-    end
-
+#write a revision statement to the record at the same time
+add_resource_revision_statement(uri, "Updated use restriction.")
+post = @client.post(uri, resource.to_json)
+puts post.body
+File.write(log, post.body, mode: 'a')
+rescue Exception => msg
+error = "Process ended: #{Time.now} with error '#{msg.class}: #{msg.message}''"
+puts error
 end
 
 end_time = "Process ended: #{Time.now}"
