f62e040ea18970e9f4271fae6189ec07c408048f
regineheberlein <regineheberlein@gmail.com>
2023-12-20 13:58:08 -0500

    rubocop

--- data_fixes/C1449/edit_accessrestrict.rb
+++ data_fixes/C1449/edit_accessrestrict.rb
@@ -2,7 +2,8 @@ require 'archivesspace/client'
 require 'json'
 require_relative '../../helper_methods.rb'
 
-resource = "/repositories/5/resources/3838"
+resource_uri = "/repositories/5/resources/3838"
+resource = @client.get(resource_uri).parsed
 phrase = /RBSC's/
 input_file = "input.csv"
 csv = CSV.parse(File.read(input_file), :headers => true)
@@ -21,12 +22,10 @@ csv.each do |row|
     #replace the offending string if found
     accessrestrict_all.each do |accessrestrict|
         note_text = accessrestrict['subnotes'][0]['content']
-        puts "old: #{note_text}"
-        next if accessrestrict.empty?      
+        next if accessrestrict.empty?
+
         if note_text.match(phrase)
-            note_text = note_text.gsub(phrase,"the RBSC")
-            puts "new: #{note_text}"
-        else ()
+            note_text = note_text.gsub(phrase, "the RBSC")
         end
     end
     post = @client.post(uri, record.to_json)
@@ -39,11 +38,10 @@ csv.each do |row|
 end
 
 #write a revision statement to the record at the same time
-resource = $client.get(resource_uri).parsed
 add_resource_revision_statement(resource, "Reworded accessrestrict without apostrophe to avoid known requesting issue.")
 post = @client.post(resource_uri, resource.to_json)
 puts post.body
-File.write(log, post.body, mode: 'a')    
+File.write(log, post.body, mode: 'a')
 
 end_time = "Process ended: #{Time.now}"
 puts end_time
