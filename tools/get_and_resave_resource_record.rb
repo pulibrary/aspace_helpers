abd3b0155b4af473d80f384e9a47f2064c191914
regineheberlein <regineheberlein@gmail.com>
2023-12-29 08:50:38 -0500

    get all resource records and re-save them to measure how long it takes to re-index

---
+++ tools/get_and_resave_resource_record.rb
@@ -0,0 +1,25 @@
+require 'archivesspace/client'
+require 'active_support/all'
+require_relative 'helper_methods.rb'
+
+
+@client = aspace_login
+
+log = "log_benchmark.txt"
+repos = (12..12).to_a
+
+File.write(log, Time.now, mode: 'a')
+
+#iterate over repositories
+repos.each do |repo|
+#iterate over all resources within repositories
+resources = get_all_records_for_repo_endpoint(repo, "resources")
+  resources.each do |resource|
+    uri = resource['uri']
+    post = @client.post(uri, resource.to_json)
+    puts post.body
+    File.write(log, post.body, mode: 'a')
+  end
+end
+
+File.write(log, Time.now, mode: 'a')
