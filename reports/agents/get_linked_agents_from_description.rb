e01d8e293640fad9f277277999a8f91097e613f5
regineheberlein <regineheberlein@gmail.com>
2024-01-12 17:58:20 -0500

    get agents linked from resource and ao records (in progress)

---
+++ reports/agents/get_linked_agents_from_description.rb
@@ -0,0 +1,28 @@
+require 'archivesspace/client'
+require 'active_support/all'
+require 'nokogiri'
+require_relative 'helper_methods.rb'
+
+aspace_login
+
+#get all resource uri's
+resource_uris = get_all_resource_uris_for_institution
+#get uri's for all ao's (published only)
+all_uris = []
+resource_uris[0..1].each do |resource_uri|
+  all_uris << @client.get("#{resource_uri}/ordered_records").parsed
+end
+all_uris = all_uris.flatten
+#get each individual uri
+refs = []
+all_uris.each do |hash|
+  uris = hash['uris']
+  refs << uris.map{ |uri| uri['ref'] }
+end
+refs = refs.flatten
+refs[0..1].each do |ref|
+  puts @client.get(ref).parsed
+end
+# resolve agents and subjects
+# [{uris => {'ref'=>'123'}, {'ref'=>'234'}}, {uris => {'ref'=>'345'}, {'ref'=>'456'}}]
+#report out agent/subject uri, type, resource/ao uri
