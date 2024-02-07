e8bff81724f98133cb53a3ad30ac958956d6cb89
regineheberlein <regineheberlein@gmail.com>
2024-02-07 13:49:46 -0500

    delete only the subset of unique ids; add terms only if the spreadsheet has them

--- data_fixes/agents_remediation/subjects2existing_agent.rb
+++ data_fixes/agents_remediation/subjects2existing_agent.rb
@@ -17,86 +17,60 @@ aspace_staging_login
 puts Time.now
 
 csv = CSV.parse(File.read("input.csv"), :headers => true)
-term1 = "Alumni and alumnae"
-term1type = "topical"
-term2 = ""
-term2type = ""
 agent_ref = "/agents/corporate_entities/1942" #/corporate_entities/1942 (Princeton University)
 
-#1. for each row:
-#2. delete subject and agent record as appropriate
+#1. gather subjects and agents to delete
+@deletes = []
+#2. for each row:
 csv.each do |row|
-    delete = @client.delete(row['heading_uri'])
-    puts delete.body
+    @deletes << row['heading_uri']
+    # get the descriptive record
     record = @client.get(row['record_uri']).parsed
     # link to agent and apply subject role and terms
     agent = {
-        "role"=>"subject", 
+        "role"=>"subject",
         "terms"=>[
-        if term1.blank? == false
+            if row['term1'].blank? == false
             {
-                "term"=>term1, 
-                "term_type"=>term1type, 
+                "term"=>row['term1'], 
+                "term_type"=>row['term1type'], 
                 "jsonmodel_type"=>"term",
                 "vocabulary"=>"/vocabularies/1"
             }
-            if term2.blank? == false
-                {
-                    "term"=>term2, 
-                    "term_type"=>term2type, 
-                    "jsonmodel_type"=>"term",
-                    "vocabulary"=>"/vocabularies/1"
-                }
+            end,
+            if row['term2'].blank? == false
+            {
+                "term"=>row['term2'], 
+                "term_type"=>row['term2type'], 
+                "jsonmodel_type"=>"term",
+                "vocabulary"=>"/vocabularies/1"
+            }
+            end,
+            if row['term3'].blank? == false
+            {
+                "term"=>row['term3'], 
+                "term_type"=>row['term3type'], 
+                "jsonmodel_type"=>"term",
+                "vocabulary"=>"/vocabularies/1"
+            }
             end
-        end
-    ],
+        ],
     "ref" => agent_ref
     }
-    # link to agent record
-    record['linked_agents'] << agent
-    post = @client.post(row['record_uri'], record.to_json)
-    puts post.body
+    #puts agent
+    unless record['linked_agents'].nil?
+        # link to agent record
+        record['linked_agents'] << agent 
+        post = @client.post(row['record_uri'], record.to_json)
+        puts post.body
+    end 
+end
+#delete false subject headings
+@deletes = @deletes.uniq
+@deletes.each do |uri_to_delete|
+    @client.delete(uri_to_delete)
+    puts delete.body
 end
 
 puts Time.now
 
-# {"role"=>"subject", 
-#     "terms"=>[
-#         {
-#         "id"=>126, 
-#         "lock_version"=>0, 
-#         "json_schema_version"=>1, 
-#         "vocab_id"=>1, 
-#         "term"=>"Students", 
-#         "term_type_id"=>1275, 
-#         "created_by"=>"admin", 
-#         "last_modified_by"=>"admin", 
-#         "create_time"=>"2016-06-27T14:39:43Z", 
-#         "system_mtime"=>"2016-06-27T14:39:43Z", 
-#         "user_mtime"=>"2016-06-27T14:39:43Z", 
-#         "x_foreign_key_x"=>489715, 
-#         "term_type"=>"topical", 
-#         "jsonmodel_type"=>"term", 
-#         "uri"=>"/terms/126", 
-#         "vocabulary"=>"/vocabularies/1"
-#         }, 
-#         {
-#         "id"=>17868, 
-#         "lock_version"=>0, 
-#         "json_schema_version"=>1, 
-#         "vocab_id"=>1, 
-#         "term"=>"20th century", 
-#         "term_type_id"=>1274, 
-#         "created_by"=>"kbolding", 
-#         "last_modified_by"=>"kbolding", 
-#         "create_time"=>"2021-02-10T19:32:00Z", 
-#         "system_mtime"=>"2021-02-10T19:32:00Z", 
-#         "user_mtime"=>"2021-02-10T19:32:00Z", 
-#         "x_foreign_key_x"=>489715, 
-#         "term_type"=>"temporal", 
-#         "jsonmodel_type"=>"term", 
-#         "uri"=>"/terms/17868", 
-#         "vocabulary"=>"/vocabularies/1"
-#         }
-#     ]
-# , "ref"=>"/agents/corporate_entities/1942"}
