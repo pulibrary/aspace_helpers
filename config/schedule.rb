c5ee9f72f386f142cc6e945c3d8350b8fb1fde2b
Max Kadel <mkadel@princeton.edu>
2024-03-27 10:00:36 -0400

    Deploy to all servers (#528)
    - Only put cron jobs on lib-jobs-prod2
    
    Co-authored-by: regineheberlein <regineheberlein@users.noreply.github.com>

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,12 +21,12 @@
 
 # Run on production at 03:30 am EST or 2:30 am EDT every morning except
 # Monday (Alma jobs are backed up) or Saturday (ASpace maintenance window)
-every '30 8 * * 2-5,7', roles: [:prod] do
+every '30 8 * * 2-5,7', roles: [:cron] do
   command "cd /opt/aspace_helpers/current/reports/aspace2alma && bundle exec ruby get_MARCxml.rb"
 end
 
 # Run on production at 03:30 am EST or 2:30 am EDT on days we are skipping aspace2alma:
 # Monday (Alma jobs are backed up) or Saturday (ASpace maintenance window)
-every '30 8 * * 1,6', roles: [:prod] do
+every '30 8 * * 1,6', roles: [:cron] do
   command "cd /opt/aspace_helpers/current/reports/aspace2alma && bundle exec ruby delete_from_sftp.rb"
 end
