66b278df16e387983c26a2bc508f717a09bd4d73
regineheberlein <regineheberlein@users.noreply.github.com>
2024-07-31 13:57:21 -0400

    add terms supplied by CaMS, add mdash (#573)
    * add terms supplied by CaMS, add mdash
    
    * rubocop

--- data_fixes/subject_remediation/fix_subfield_v.rb
+++ data_fixes/subject_remediation/fix_subfield_v.rb
@@ -6,7 +6,40 @@ require_relative '../../helper_methods.rb'
 
 puts Time.now
 
-genreterms = ["Correspondence", "Photographs", "Manuscripts"]
+genreterms = ["Archives",
+              "Bibliography",
+              "Biography",
+              "Caricatures and cartoons",
+              "Catalogs",
+              "Correspondence",
+              "Designs and plans",
+              "Dictionaries",
+              "Drama",
+              "Drawings",
+              "Early works to 1800",
+              "Facsimiles",
+              "Fiction",
+              "Folklore",
+              "In art",
+              "Manuscripts Facsimiles",
+              "Maps",
+              "Miscellanea",
+              "Newspapers",
+              "Pamphlets",
+              "Periodicals",
+              "Photographs",
+              "Pictorial works",
+              "Posters",
+              "Prayers and devotions",
+              "Records and correspondence",
+              "Slides",
+              "Songs and music",
+              "Sources",
+              "Specimens",
+              "Statistics",
+              "Textbooks",
+              "Texts",
+              "Translations"]
 
 subject_ids = @client.get(
   "/subjects", { query: { all_ids: true } }
@@ -16,14 +49,14 @@ subjects_all =
 
 genreterms.each do |genreterm|
     subjects_correspondence = subjects_all.select do |subject|
-            subject['title'] =~ /--\s?#{genreterm}\.?\s?$/
+            subject['title'] =~ /(--|—)\s?#{genreterm}\.?\s?$/
         end
 
     subjects_correspondence.map do |subject|
         uri = subject['uri']
-        next unless subject['terms'][0]['term'] =~ /--\s?#{genreterm}/
+        next unless subject['terms'][0]['term'] =~ /(--|—)\s?#{genreterm}/
 
-        subject['terms'][0]['term'] = subject['terms'][0]['term'].gsub(/(^.+?)(\s?--\s?#{genreterm})(.+?$)/, '\1\3')
+        subject['terms'][0]['term'] = subject['terms'][0]['term'].gsub(/(^.+?)(\s?(--|—)\s?)(#{genreterm})(.*?$)/, '\1\5')
         subject['terms'] << {"term"=>genreterm.capitalize.to_s, "term_type"=>"genre_form", "jsonmodel_type"=>"term", "vocabulary"=>"/vocabularies/1"}
         add_maintenance_history(subject, "Subjects remediation: move genre terms to $v")
         post = @client.post(uri, subject.to_json)
