fa73ab9d9517ea1fd8f53a39ecf3b7f442616f51
regineheberlein <regineheberlein@gmail.com>
2024-08-09 18:05:35 -0400

    rubocop and remove unneeded variable

--- data_fixes/edit_userestrict/update_links_userestrict.rb
+++ data_fixes/edit_userestrict/update_links_userestrict.rb
@@ -8,25 +8,6 @@ start_time = "Process started: #{Time.now}"
 puts start_time
 log = "log_userestrict.txt"
 
-# userestrict_note = "<p>Single copies may be made for research purposes. To cite or publish quotations
-# that fall within Fair Use, as defined under <extref
-# xlink:href='http://copyright.princeton.edu/basics/fair-use' xlink:type='simple'>U. S. Copyright Law</extref>,
-# no permission is required. The Trustees of Princeton University hold copyright to all materials generated
-# by Princeton University employees in the course of their work.  For instances beyond Fair Use,
-# if copyright is held by Princeton University, researchers do not need to obtain permission, complete any
-# forms, or receive a letter to move forward with use of materials from the Princeton University Archives.</p>
-# <p>For instances beyond Fair Use where the copyright is not held by the University, while  permission from
-# the Library is not required, it is the responsibility of the researcher to determine whether any permissions
-# related to copyright, privacy, publicity, or any other rights are necessary for their intended use of the
-# Library's materials, and to obtain all required permissions from any existing rights holders, if they have
-# not already done so. Princeton University Library’s Special Collections does not charge any permission or
-# use fees for the publication of images of materials from our collections, nor does it require researchers
-# to obtain its permission for said use. The department does request that its collections be properly cited
-# and images credited. More detailed information can be found on the <extref
-# xlink:href='https://library.princeton.edu/services/special-collections/explore-special-collections'
-# xlink:type='simple'>Copyright, Credit and Citations Guidelines</extref> page on our website. If you have any
-# questions, please feel free to contact us through the <extref
-# xlink:href='https://library.princeton.edu/services/special-collections/ask-special-collections' xlink:type='simple'>Ask Us! form</extref>.</p>"
 repos_all = (12..12).to_a
 
 #iterate over all repositories
@@ -42,9 +23,8 @@ resources = get_all_records_for_repo_endpoint(repo, "resources")
         if userestrict['subnotes'][0]['content'].include? "https://library.princeton.edu/special-collections/ask-us"
             userestrict['subnotes'][0]['content'] = userestrict['subnotes'][0]['content'].gsub("https://library.princeton.edu/special-collections/ask-us", "https://library.princeton.edu/services/special-collections/ask-special-collections")
         end
-        if
-            userestrict['subnotes'][0]['content'].include? "https://library.princeton.edu/special-collections/policies/copyright-credit-and-citation-guidelines"
-            userestrict['subnotes'][0]['content'] = userestrict['subnotes'][0]['content'].gsub("https://library.princeton.edu/special-collections/policies/copyright-credit-and-citation-guidelines", "test" )
+        if userestrict['subnotes'][0]['content'].include? "https://library.princeton.edu/special-collections/policies/copyright-credit-and-citation-guidelines"
+            userestrict['subnotes'][0]['content'] = userestrict['subnotes'][0]['content'].gsub("https://library.princeton.edu/special-collections/policies/copyright-credit-and-citation-guidelines", "test")
         end
     end
     #write a revision statement to the record at the same time
