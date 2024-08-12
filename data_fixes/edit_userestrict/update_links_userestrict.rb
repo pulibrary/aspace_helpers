e9393bbc7d46915ed03ac73dbc4896b14369c5a6
regineheberlein <regineheberlein@gmail.com>
2024-08-12 14:47:31 -0400

    change the whole sentence, including the link

--- data_fixes/edit_userestrict/update_links_userestrict.rb
+++ data_fixes/edit_userestrict/update_links_userestrict.rb
@@ -2,13 +2,19 @@ require 'archivesspace/client'
 require 'json'
 require_relative '../../helper_methods.rb'
 
-aspace_login
+aspace_staging_login
 
 start_time = "Process started: #{Time.now}"
 puts start_time
 log = "log_userestrict.txt"
+old_text = "More detailed information can be found on the <extref
+xlink:href='https://library.princeton.edu/special-collections/policies/copyright-credit-and-citation-guidelines'
+xlink:type='simple'>Copyright, Credit and Citations Guidelines</extref> page on our website."
+new_text = "More detailed information can be found on the Copyright, Credit and Citations Guidelines section of  <extref
+xlink:href='https://library.princeton.edu/services/special-collections/explore-special-collections'
+xlink:type='simple'>Explore our Collections</extref>."
 
-repos_all = (3..12).to_a
+repos_all = (11..11).to_a
 
 #iterate over all repositories
 repos_all.each do |repo|
@@ -23,8 +29,9 @@ resources = get_all_records_for_repo_endpoint(repo, "resources")
         if userestrict['subnotes'][0]['content'].include? "https://library.princeton.edu/special-collections/ask-us"
             userestrict['subnotes'][0]['content'] = userestrict['subnotes'][0]['content'].gsub("https://library.princeton.edu/special-collections/ask-us", "https://library.princeton.edu/services/special-collections/ask-special-collections")
         end
-        if userestrict['subnotes'][0]['content'].include? "https://library.princeton.edu/special-collections/policies/copyright-credit-and-citation-guidelines"
-            userestrict['subnotes'][0]['content'] = userestrict['subnotes'][0]['content'].gsub("https://library.princeton.edu/special-collections/policies/copyright-credit-and-citation-guidelines", "test")
+        if userestrict['subnotes'][0]['content'].include? old_text.gsub(/\n/, " ")
+            
+            userestrict['subnotes'][0]['content'] = userestrict['subnotes'][0]['content'].gsub(old_text.gsub(/\n/, " "), new_text.gsub(/\n/, " "))
         end
     end
     #write a revision statement to the record at the same time
