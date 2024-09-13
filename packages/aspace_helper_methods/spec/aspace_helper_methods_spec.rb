5b64183f706149e9624e42e65a4081642f880ae4
Max Kadel <mkadel@princeton.edu>
2024-09-13 15:39:19 -0400

    Add scaffolding for aspace_helper_methods gem
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: regineheberlein <regineheberlein@users.noreply.github.com>

---
+++ packages/aspace_helper_methods/spec/aspace_helper_methods_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AspaceHelperMethods do
+  it "has a version number" do
+    expect(AspaceHelperMethods::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
