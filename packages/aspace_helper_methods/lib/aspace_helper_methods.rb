0558ca5854371766c00f3602eab7184855835602
Jane Sandberg <js7389@princeton.edu>
2024-09-13 12:57:29 -0700

    Move helper_methods and a test to the new aspace_helper_methods gem
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>
    Co-authored-by: regineheberlein <regineheberlein@users.noreply.github.com>

--- packages/aspace_helper_methods/lib/aspace_helper_methods.rb
+++ packages/aspace_helper_methods/lib/aspace_helper_methods.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "aspace_helper_methods/version"
+require_relative "aspace_helper_methods/helper_methods"
 
 module AspaceHelperMethods
   class Error < StandardError; end
