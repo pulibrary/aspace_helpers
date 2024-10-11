6772fb7b9139abe59e4b5f55283e924985c9bd9c
regineheberlein <regineheberlein@users.noreply.github.com>
2024-10-11 16:25:27 -0400

    Update digital_objects.rb
    remove comment

--- reports/digital_objects/digital_objects.rb
+++ reports/digital_objects/digital_objects.rb
@@ -13,24 +13,6 @@ repos_all.each do |repo|
   dos << get_all_digital_object_records_for_a_repository(repo)
 end
 dos = dos.flatten!
-# puts dos.class
-# puts dos[0]
-# puts dos[0].class
-# puts dos[0]['collection']
-# puts dos[0]['collection'].empty?
-# puts dos[0]['collection'].class
-# puts dos.count
-# puts dos[0]
-# puts dos[0]['collection'].class
-#   if dos[0]['collection'].nil?
-#     puts "nil"
-#   elsif dos[0]['collection'].empty?
-#     puts "empty"
-#   elsif dos[0]['collection'].blank?
-#     puts "blank"
-#   else
-#     puts "don't know what to do with #{dos[0]['collection']}"
-#   end
 
 select_dos =
   dos.select do |dobject|
