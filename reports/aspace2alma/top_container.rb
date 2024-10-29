1e80460686ecf10563f06e9f480495aabfebf9a5
regineheberlein <regineheberlein@gmail.com>
2024-10-29 15:03:36 -0400

    start working on a container class and corresponding test
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>

---
+++ reports/aspace2alma/top_container.rb
@@ -0,0 +1,21 @@
+require 'nokogiri'
+require 'active_support/all'
+require 'archivesspace/client'
+
+class TopContainer
+
+  # attr_reader :resource_uri, :aspace_client
+
+  # def initialize(resource_uri, aspace_client, _file, _log_out, _remote_file)
+  #   @resource_uri = resource_uri
+  #   @aspace_client = aspace_client
+  # end
+
+  def initialize(container_doc)
+  end
+
+  def resource_uri
+    #container['collection_uri_u_sstr']
+  end
+
+end
---
+++ spec/reports/aspace2alma/top_container_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: false
+
+require_relative '../../../reports/aspace2alma/top_container'
+require 'spec_helper.rb'
+
+RSpec.describe TopContainer do
+  let(:container_doc) { "{}" }
+  let(:container_instance) { described_class.new(container_doc) }
+  it 'can be instantiated' do
+    #expect(described_class.new(resource_uri, client, 'file', 'log_out', 'remote_file')).to be
+    expect(described_class.new(container_doc))
+  end
+  it 'has a resource uri' do
+    expect(container_instance.resource_uri)
+  end
+
+end
