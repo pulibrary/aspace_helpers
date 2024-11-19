649cb9fb46d7c37f912a35a1b4b0ace5b30e3f51
regineheberlein <regineheberlein@gmail.com>
2024-11-19 14:55:21 -0500

    add container location
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>

--- spec/reports/aspace2alma/top_container_spec.rb
+++ spec/reports/aspace2alma/top_container_spec.rb
@@ -4,13 +4,19 @@ require_relative '../../../reports/aspace2alma/top_container'
 require 'spec_helper.rb'
 
 RSpec.describe TopContainer do
-  let(:container_doc) { '{"collection_uri_u_sstr": ["/repositories/12/resources/1467"]}' }
-  let(:container_instance) { described_class.new(container_doc) }
+  let(:container) { JSON.parse(File.read(File.open('spec/fixtures/single_container.json'))) }
+  let(:container_instance) { described_class.new(container) }
   it 'can be instantiated' do
     #expect(described_class.new(resource_uri, client, 'file', 'log_out', 'remote_file')).to be
-    expect(described_class.new(container_doc))
+    expect(described_class.new(container))
   end
-  it 'has a resource uri' do
+  it 'has a container' do
     expect(container_instance.container_doc).to be_an_instance_of Hash
+  end
+  it 'has a location code' do
+    expect(container_instance.location_code).to be_an_instance_of String
+  end
+  it 'has the correct location code' do
+    expect(container_instance.location_code).to eq "scarcpph"
   end
 end
