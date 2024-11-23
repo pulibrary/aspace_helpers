88b31513719daf33bb6fcae6c96dc75ac72ea924
regineheberlein <regineheberlein@gmail.com>
2024-11-23 14:00:16 -0500

    put genre/form last

---
+++ data_fixes/subject_remediation/re-order_subject_subdivisions.rb
@@ -0,0 +1,30 @@
+require 'archivesspace/client'
+require 'active_support/all'
+require 'nokogiri'
+require_relative '../../helper_methods.rb'
+
+puts Time.now
+
+aspace_login
+
+subject_ids = @client.get(
+  "/subjects", { query: { all_ids: true } }
+).parsed
+
+subjects =
+  subject_ids.map {|subject_id| @client.get("/subjects/#{subject_id}").parsed}
+
+subjects.each do |subject|
+  next if subject['terms'].count <2
+  next if subject['terms'][1]['term_type'] != "genre_form"
+  uri = subject['uri']
+  if subject['terms'][1]['term_type'] == "genre_form"
+    terms = [subject['terms'][0]]
+    subject['terms'][1..].reverse.map {|term| terms << term}
+    subject['terms'] = terms
+    add_maintenance_history(subject, "Subjects remediation: put genre/form last")
+    post = @client.post(uri, subject.to_json)
+  end
+end
+
+puts Time.now
