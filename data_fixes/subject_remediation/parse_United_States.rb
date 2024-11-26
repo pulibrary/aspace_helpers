e56af401ab0f9d0e23546a9c925e7756efb241b3
regineheberlein <regineheberlein@gmail.com>
2024-11-26 17:00:55 -0500

    rubocop

--- data_fixes/subject_remediation/parse_United_States.rb
+++ data_fixes/subject_remediation/parse_United_States.rb
@@ -15,14 +15,14 @@ subjects_all =
   subject_ids.map {|subject_id| @client.get("/subjects/#{subject_id}").parsed}
 
 subjects = []
-subjects << subjects_all.select do |subject| 
+subjects << subjects_all.select do |subject|
     subject['terms'][0]['term'] =~ /--\s?United States/i
   end
 
 subjects.flatten!.each do |subject|
-  puts "#{subject['uri']}^#{subject['terms'].map { |term| term['term'] + "^" + term['term_type']}.join('^')}"  
+  puts "#{subject['uri']}^#{subject['terms'].map { |term| "#{term['term']}^#{term['term_type']}"}.join('^')}"
 end
-  # parse_subject_string = 
+# parse_subject_string =
 #   subjects.each do |subject|
 #   terms = [subject['terms'][0]]
 
