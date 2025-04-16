207e4aa1536a95590ed605c99a457bce5fcdccb0
regineheberlein <regineheberlein@users.noreply.github.com>
2025-04-16 12:30:50 -0400

    Update get_aos_with_specific_acqinfo.sql
    edited to start with the ao and include those that don't have boxes; also sorted by ao id to simulate document order

--- reports/MC104/get_aos_with_specific_acqinfo.sql
+++ reports/MC104/get_aos_with_specific_acqinfo.sql
@@ -6,13 +6,13 @@ SELECT DISTINCT
   , tc.indicator as box_number
   , MAX(CASE WHEN n.notes LIKE "%accessrestrict%" THEN REGEXP_REPLACE(n.notes, '(.+content\":\")(.+?)(\",\".+)', '$2') END) as accessrestrict
   , MAX(CASE WHEN n.notes LIKE "%acqinfo%" THEN REGEXP_REPLACE(n.notes, '(.+content\":\")(.+?)(\",\".+)', '$2') END) as acqinfo
-FROM sub_container sc
-JOIN top_container_link_rlshp tclr on tclr.sub_container_id = sc.id
-JOIN top_container tc on tclr.top_container_id = tc.id
-JOIN instance on sc.instance_id = instance.id
-JOIN archival_object ao on instance.archival_object_id = ao.id
-LEFT JOIN note n on n.archival_object_id  = ao.id
+FROM archival_object ao 
+LEFT JOIN note n on ao.id = n.archival_object_id
+LEFT JOIN instance i on ao.id = i.archival_object_id
+LEFT JOIN sub_container sc on i.id = sc.instance_id
+LEFT JOIN top_container_link_rlshp tclr on sc.id = tclr.sub_container_id
+LEFT JOIN top_container tc on tclr.top_container_id = tc.id
 WHERE ao.root_record_id = 1718 
 GROUP BY ao.id
 HAVING acqinfo LIKE '%ML-2025-002%'
-ORDER BY ao.ref_id
+ORDER BY ao.id
