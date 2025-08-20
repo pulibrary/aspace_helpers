721d0d4c8e97596b2d4e65c92b5ae1cb2a4c8eff
regineheberlein <regineheberlein@gmail.com>
2025-08-20 08:33:07 -0400

    report out local agents for NACO training

---
+++ reports/agents/get_local_agents.rb
@@ -0,0 +1,37 @@
+require 'archivesspace/client'
+require 'active_support/all'
+require_relative '../../helper_methods.rb'
+require 'csv'
+require 'json'
+
+@client = aspace_login
+
+puts Time.now
+
+    ['people', 'corporate_entities', 'families'].each do |agent_type|
+        ids = []
+        ids << @client.get("/agents/#{agent_type}", {
+            query: {
+            all_ids: true
+                }
+            }).parsed
+        ids = ids.flatten
+
+        ids.each do |id|
+            record = @client.get("/agents/#{agent_type}/#{id}").parsed
+            next if record.nil?
+            if record.dig('names', 0, 'source').blank? || record.dig('names', 0, 'source') == 'local'
+                uri = record['uri']
+                source = record['names'][0]['source'] ||= ''
+                rules = record['names'][0]['rules'] ||= ''
+                authority = record['names'][0]['authority_id'] ||= ''
+                unless record['agent_identifiers'].blank?
+                    entity_id = record.dig('agent_identifiers', 0, 'entity_identifier')
+                end
+                title = record['title']
+                puts "#{uri}^#{title ||= ''}^#{source ||= ''}^#{rules ||= ''}^#{authority ||= ''}^#{entity_id ||= ''}"
+            end
+        end
+    end
+
+puts Time.now
