9ff43c4b3388aac9cffae82202dd4c5dc10cadee
regineheberlein <regineheberlein@gmail.com>
2025-08-20 08:36:25 -0400

    rubocop

--- reports/agents/get_local_agents.rb
+++ reports/agents/get_local_agents.rb
@@ -11,26 +11,27 @@ puts Time.now
     ['people', 'corporate_entities', 'families'].each do |agent_type|
         ids = []
         ids << @client.get("/agents/#{agent_type}", {
-            query: {
-            all_ids: true
-                }
-            }).parsed
+                             query: {
+                               all_ids: true
+                             }
+                           }).parsed
         ids = ids.flatten
 
         ids.each do |id|
             record = @client.get("/agents/#{agent_type}/#{id}").parsed
             next if record.nil?
-            if record.dig('names', 0, 'source').blank? || record.dig('names', 0, 'source') == 'local'
-                uri = record['uri']
-                source = record['names'][0]['source'] ||= ''
-                rules = record['names'][0]['rules'] ||= ''
-                authority = record['names'][0]['authority_id'] ||= ''
-                unless record['agent_identifiers'].blank?
-                    entity_id = record.dig('agent_identifiers', 0, 'entity_identifier')
-                end
-                title = record['title']
-                puts "#{uri}^#{title ||= ''}^#{source ||= ''}^#{rules ||= ''}^#{authority ||= ''}^#{entity_id ||= ''}"
+
+            next unless record.dig('names', 0, 'source').blank? || record.dig('names', 0, 'source') == 'local'
+
+            uri = record['uri']
+            source = record['names'][0]['source'] ||= ''
+            rules = record['names'][0]['rules'] ||= ''
+            authority = record['names'][0]['authority_id'] ||= ''
+            unless record['agent_identifiers'].blank?
+                entity_id = record.dig('agent_identifiers', 0, 'entity_identifier')
             end
+            title = record['title']
+            puts "#{uri}^#{title ||= ''}^#{source ||= ''}^#{rules ||= ''}^#{authority ||= ''}^#{entity_id ||= ''}"
         end
     end
 
