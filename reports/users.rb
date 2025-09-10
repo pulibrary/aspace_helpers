fd876a8f6466f2dbb9939a67ed3d5017f8068e3a
Regine Heberlein <regineheberlein@users.noreply.github.com>
2025-09-10 15:08:30 -0400

    get users, user status, repos, and permissions (#725)
    * get users, user status, repos, and permissions

---
+++ reports/users.rb
@@ -0,0 +1,28 @@
+require 'archivesspace/client'
+require 'active_support/all'
+require_relative 'helper_methods.rb'
+require 'csv'
+
+@client = aspace_login
+
+output_file = "users.csv"
+CSV.open(output_file, "w",
+         :write_headers => true,
+         :headers => ["username", "name", "is active user", "repositories", "permissions"]) do |row|
+    users = @client.get("/users", {
+                          query: {
+                            all_ids: true
+                          }
+                        }).parsed
+    # puts users
+
+    users.each do |id|
+    user = @client.get("/users/#{id}").parsed
+    repositories = user['permissions'].keys.join(', ')
+    permissions =
+      user['permissions'].map do |k, v|
+          "#{k}: #{v.join(', ')}"
+      end
+    row << [user['username'], user['name'], user['is_active_user'], repositories, permissions[0]]
+    end
+end
