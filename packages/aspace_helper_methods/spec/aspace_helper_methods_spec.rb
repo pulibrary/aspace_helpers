5da3edc5e0009e2745a424e4226e33f4bc70b62e
Christina Chortaria <actspatial@gmail.com>
2025-09-29 12:44:01 -0400

    run `bundle exec rubocop -A`

--- packages/aspace_helper_methods/spec/aspace_helper_methods_spec.rb
+++ packages/aspace_helper_methods/spec/aspace_helper_methods_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe AspaceHelperMethods do
   it "has a version number" do
-    expect(AspaceHelperMethods::VERSION).not_to be nil
+    expect(AspaceHelperMethods::VERSION).not_to be_nil
   end
 end
