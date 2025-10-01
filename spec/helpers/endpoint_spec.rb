2fa6f5d7e4a47d82414f949ca8777bb20a77d5ee
regineheberlein <regineheberlein@gmail.com>
2025-10-01 10:32:25 -0400

    add expectation

--- spec/helpers/endpoint_spec.rb
+++ spec/helpers/endpoint_spec.rb
@@ -18,13 +18,12 @@ RSpec.describe 'endpoint methods' do
 
   it 'can retrieve an agent by id' do
   stub_request(:get, "https://example.com/staff/api/agents/people/11175").
-         with(
-           headers: {
-       	  'Accept'=>'*/*',
-       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	  'User-Agent'=>'ArchivesSpaceClient/0.4.1',
-       	  'X-Archivesspace-Session'=>'session'
-           }).
-         to_return(status: 200, body: "", headers: {})
+    with(headers: {
+           'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'ArchivesSpaceClient/0.4.1',
+      'X-Archivesspace-Session'=>'session'
+         }).to_return(status: 200, body: "", headers: {})
+  expect(get_agent_stub).to have_been_requested
   end
 end
