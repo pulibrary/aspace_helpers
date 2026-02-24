90191a208e50ac0bbec66370baa83bcde5de7453
regineheberlein <regineheberlein@gmail.com>
2026-02-24 09:12:03 -0500

    rubocop

--- data_fixes/LAE/merge_collection_as_series.rb
+++ data_fixes/LAE/merge_collection_as_series.rb
@@ -9,9 +9,9 @@ aspace_staging_login
 puts "Process started: #{Time.now}"
 
 merge_destination_id = 4083
-merge_destination_record = @client.get('/repositories/8/resources/'+merge_destination_id.to_s, query: {
-                resolve: []
-              }).parsed
+merge_destination_record = @client.get("/repositories/8/resources/#{merge_destination_id}", query: {
+                                         resolve: []
+                                       }).parsed
 merge_destination_uri = merge_destination_record['uri']
 merge_candidate_uris = ['/repositories/8/resources/4084']
 
@@ -19,7 +19,7 @@ merge_candidate_uris = ['/repositories/8/resources/4084']
 merge_candidate_uris.each do |candidate_uri|
   candidate_record = @client.get(candidate_uri).parsed
   post = @client.post('/repositories/8/archival_objects', {
-      title: candidate_record['title'],
+                        title: candidate_record['title'],
       level: 'series',
       publish: true,
       external_ids: candidate_record['external_ids'],
@@ -36,41 +36,37 @@ merge_candidate_uris.each do |candidate_uri|
       #'ancestors': [],
       instances: candidate_record['instances'],
       notes: candidate_record['notes'],
-      #'ref_id': "20029191", 
-      resource: {'ref': merge_destination_uri}
- })
+      #'ref_id': "20029191",
+      resource: {ref: merge_destination_uri}
+                      })
  puts post.body
-#  new_series_id = post.body['id']
-# get tree information for the candidate resource
-#  candidate_ao_tree = @client.get(candidate_uri+'/ordered_records').parsed['uris']
-# get the ao uris from the candidate tree and set their resource to the new destination resource
-#  all_candidate_ao_uris = candidate_ao_tree.map { |ao| ao['ref']}
-#  all_candidate_ao_uris.each do |candidate_ao_uri|
-#   candidate_ao_record = @client.get(candidate_ao_uri).parsed
-#   candidate_ao_record['resource'] = {"ref": merge_destination_uri}
-#   next if candidate_ao_record.key?('parent')
-#   candidate_ao_record['parent'] = {"ref": "/archival_objects/#{new_series_id}"}
-#   @client.post(candidate_ao_uri, candidate_ao_record)
-#  end
-#identify top-level aos and 
-# top_level_ao_uris = aos_to_move.select { |ao| ao['depth'] == 1 }.map { |ao| ao['ref'] }
-
-
-
-
+   #  new_series_id = post.body['id']
+   # get tree information for the candidate resource
+   #  candidate_ao_tree = @client.get(candidate_uri+'/ordered_records').parsed['uris']
+   # get the ao uris from the candidate tree and set their resource to the new destination resource
+   #  all_candidate_ao_uris = candidate_ao_tree.map { |ao| ao['ref']}
+   #  all_candidate_ao_uris.each do |candidate_ao_uri|
+   #   candidate_ao_record = @client.get(candidate_ao_uri).parsed
+   #   candidate_ao_record['resource'] = {"ref": merge_destination_uri}
+   #   next if candidate_ao_record.key?('parent')
+   #   candidate_ao_record['parent'] = {"ref": "/archival_objects/#{new_series_id}"}
+   #   @client.post(candidate_ao_uri, candidate_ao_record)
+   #  end
+   #identify top-level aos and
+   # top_level_ao_uris = aos_to_move.select { |ao| ao['depth'] == 1 }.map { |ao| ao['ref'] }
  end
 
 # end
 #Move existing Archival Objects to become children of a Resource
 # merge_candidate_uris.each do |candidate_uri|
-  #get the top-level ao's
-  # aos = @client.get(candidate_uri+'/tree/root').parsed
-  # #add the top-level ao uri's to an array
-  # ao_uris = []
-  # aos['precomputed_waypoints']['']['0'].each do |ao| 
-  #   ao_uris << ao['uri']
-  # end
-  #post the ao uri's to the destination resource uri
+#get the top-level ao's
+# aos = @client.get(candidate_uri+'/tree/root').parsed
+# #add the top-level ao uri's to an array
+# ao_uris = []
+# aos['precomputed_waypoints']['']['0'].each do |ao|
+#   ao_uris << ao['uri']
+# end
+#post the ao uri's to the destination resource uri
 
 # end
 
@@ -80,9 +76,8 @@ merge_candidate_uris.each do |candidate_uri|
 # The accept_children endpoint is broken, else we would do:
 #   url = "/repositories/8/resources/4083/accept_children"
 #   post = @client.post(url, data)
-  # url = "/repositories/8/resources/4083/accept_children?position=3&children[]=/repositories/8/archival_objects/1258960"
-  # post = @client.post(url, {})
-
+# url = "/repositories/8/resources/4083/accept_children?position=3&children[]=/repositories/8/archival_objects/1258960"
+# post = @client.post(url, {})
 
 #[:POST] /merge_requests/resource
 # update=client.post('/merge_requests/top_container?repo_id=8', {
@@ -98,6 +93,3 @@ merge_candidate_uris.each do |candidate_uri|
 #       target: {ref: target},
 #       victims: [{ref: victim}]
 #     }.to_json)
-
-
-
