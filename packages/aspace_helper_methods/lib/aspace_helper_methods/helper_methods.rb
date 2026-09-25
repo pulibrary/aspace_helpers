8f471638e1fc8479924f80a01bca41077a9503b7
regineheberlein <regineheberlein@gmail.com>
2026-09-25 11:30:39 -0400

    stop evey 30th object from getting fetched twice

--- packages/aspace_helper_methods/lib/aspace_helper_methods/helper_methods.rb
+++ packages/aspace_helper_methods/lib/aspace_helper_methods/helper_methods.rb
@@ -260,13 +260,13 @@ def get_resolved_objects_from_ids(repository_id, input_ids, record_type, record_
   count_processed_records = 0
   count_ids = input_ids.count
   while count_processed_records < count_ids
-    last_record = [count_processed_records + 29, count_ids].min
+    last_record = [count_processed_records + 29, count_ids - 1].min
     all_records << @client.get("/repositories/#{repository_id}/#{record_type}",
                                query: {
                                  id_set: input_ids[count_processed_records..last_record],
                                  resolve: record_types_to_prefetch
                                }).parsed
-    count_processed_records = last_record
+    count_processed_records = last_record + 1
   end
   all_records.flatten
 end
